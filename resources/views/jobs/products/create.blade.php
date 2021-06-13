785dd2c7a474b8b79d23e7a9cf4274089914f8ac
dhrubanka <dhrubankachutia@gmail.com>
2021-06-13 17:19:25 +0530

    dd

--- resources/views/jobs/products/create.blade.php
+++ resources/views/jobs/products/create.blade.php
@@ -342,22 +342,23 @@
         });
 
     </script>
+    <script>
+        var x = document.getElementById("latitude");
+
+        function getLocation() {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+          } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+          }
+        }
+
+        function showPosition(position) {
+          x.innerHTML = "Latitude: " + position.coords.latitude +
+          "<br>Longitude: " + position.coords.longitude;
+        }
+        </script>
     </body>
 
-<script>
-    var x = document.getElementById("demo");
 
-    function getLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-    }
-
-    function showPosition(position) {
-      x.innerHTML = "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude;
-    }
-    </script>
 @endsection
