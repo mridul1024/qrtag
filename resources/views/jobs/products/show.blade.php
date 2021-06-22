e41bdc29f344ce5bbfb836d173eee2bc9cb7b7f7
dhrubanka <dhrubankachutia@gmail.com>
2021-06-22 20:53:45 +0530

    map alignment

--- resources/views/jobs/products/show.blade.php
+++ resources/views/jobs/products/show.blade.php
@@ -146,7 +146,7 @@
         }
         body { -webkit-print-color-adjust: exact !important; }
 
-        #mapid { height: 400px; }
+        #mapid { height: 400px; width : 300px;}
     </style>
 
     <div class="container">
